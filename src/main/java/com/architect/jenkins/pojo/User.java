277f995832cb20ebef399a3916ff1d8e4275de8d
加文雄 <jiawenxiong@newbeescm.com>
2018-06-29 17:55:57 +0800

    增加lombok依赖

--- src/main/java/com/architect/jenkins/pojo/User.java
+++ src/main/java/com/architect/jenkins/pojo/User.java
@@ -1,25 +1,12 @@
 package com.architect.jenkins.pojo;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class User {
     private Long id;
     private String name;
     private Integer age;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Integer getAge() {
-        return age;
-    }
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
